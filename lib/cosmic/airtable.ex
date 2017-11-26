31321da420b13ea21055d97348ea18e87dc7bde4
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2017-11-26 12:14:13 -0600

    done

--- lib/cosmic/airtable.ex
+++ lib/cosmic/airtable.ex
@@ -1,10 +1,15 @@
 defmodule Shorten.AirtableCache do
   use Agent
+
   @key Application.get_env(:shorten, :airtable_key)
   @base Application.get_env(:shorten, :airtable_base)
   @table Application.get_env(:shorten, :airtable_table_name)
 
+  @interval 60_000
+
   def start_link do
+    Process.send_after(self, :update, @interval)
+
     Agent.start_link(
       fn ->
         fetch_all() |> Enum.map(&regexify/1)
@@ -14,9 +19,17 @@ defmodule Shorten.AirtableCache do
   end
 
   def update do
-    Agent.update(__MODULE__, fn _current ->
-      fetch_all() |> Enum.map(&regexify/1)
-    end)
+    receive do
+      :update ->
+        Agent.update(__MODULE__, fn _current ->
+          fetch_all() |> Enum.map(&regexify/1)
+        end)
+
+        IO.puts("Updated at #{inspect(DateTime.utc_now(0))}")
+
+        Process.send_after(self, :update, @interval)
+        update()
+    end
   end
 
   def get_all do
@@ -24,13 +37,19 @@ defmodule Shorten.AirtableCache do
   end
 
   defp fetch_all() do
-    %{body: body} = HTTPotion.get("https://api.airtable.com/v0/#{@base}/#{@table}")
+    %{body: body} =
+      HTTPotion.get("https://api.airtable.com/v0/#{@base}/#{@table}", headers: [
+        Authorization: "Bearer #{@key}"
+      ])
+
     decoded = Poison.decode!(body)
 
     records =
-      Enum.map(decoded["records"], fn %{"fields" => %{"Pattern" => from, "Destination" => to}} ->
-        {from, to}
-      end)
+      decoded["records"]
+      |> Enum.filter(fn %{"fields" => fields} -> Map.has_key?(fields, "Destination") end)
+      |> Enum.map(fn %{"fields" => %{"Pattern" => from, "Destination" => to}} ->
+           {from, to}
+         end)
 
     if Map.has_key?(decoded, "offset") do
       fetch_all(records, decoded["offset"])
@@ -40,14 +59,23 @@ defmodule Shorten.AirtableCache do
   end
 
   defp fetch_all(records, offset) do
-    HTTPotion.get("https://api.airtable.com/v0/#{@base}/#{@table}")
+    %{body: body} =
+      HTTPotion.get(
+        "https://api.airtable.com/v0/#{@base}/#{@table}",
+        headers: [
+          Authorization: "Bearer #{@key}"
+        ],
+        query: [offset: offset]
+      )
 
-    decoded = Poison.decode!(body)
+    decoded = Poison.decode!(body) |> IO.inspect()
 
     new_records =
-      Enum.map(decoded["records"], fn %{"fields" => %{"Pattern" => from, "Destination" => to}} ->
-        {from, to}
-      end)
+      decoded["records"]
+      |> Enum.filter(fn %{"fields" => fields} -> Map.has_key?(fields, "Destination") end)
+      |> Enum.map(fn %{"fields" => %{"Pattern" => from, "Destination" => to}} ->
+           {from, to}
+         end)
 
     all_records = Enum.concat(records, new_records)
 
--- web/router.ex
+++ web/router.ex
@@ -1,14 +1,6 @@
 defmodule Shorten.Router do
   use Shorten.Web, :router
 
-  pipeline :api do
-    plug(:accepts, ["json"])
-  end
-
-  scope "/api", Shorten do
-    get("/update/airtable", UpdateController, :cosmic)
-  end
-
   scope "/", Shorten do
     get("/*path", GetController, :get)
   end
