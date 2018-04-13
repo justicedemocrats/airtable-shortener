cfed211d96529520b0d0dff853a3a1def9e76f67
ben-pr-p <ben.paul.ryan.packer@gmail.com>
2018-04-13 11:08:39 -0400

    add ?source=shortlink to auto event redirects

--- web/controllers/get_controller.ex
+++ web/controllers/get_controller.ex
@@ -33,53 +33,53 @@ defmodule Shorten.GetController do
         end)
         |> Enum.join("\n")
 
-      text conn, ~s(
+      text(conn, ~s(
         hello shortlink user! here are the current routes.
 
         to force an update, visit jdems.us/admin/update?secret=#{secret()}.\n\n#{resp}
-      )
+      ))
     else
-      text conn, ~s(
+      text(conn, ~s(
         wrong secret – contact ben.
-      )
+      ))
     end
   end
 
   def admin(conn, _) do
-    text conn, ~s(
+    text(conn, ~s(
       missing secret – proper usage is jdems.us/admin?secret=secretfromben
-    )
+    ))
   end
 
   def update(conn, %{"secret" => input_secret}) do
     if input_secret == secret() do
       Shorten.AirtableCache.update()
 
-      text conn, ~s(
+      text(conn, ~s(
         hello shortlink user! we just updated. to verify that your update
         has completed or to further diagnose issues, visit
 
         -> jdems.us/admin?secret=#{secret()}.
-      )
+      ))
     else
-      text conn, ~s(
+      text(conn, ~s(
         wrong secret – contact ben.
-      )
+      ))
     end
   end
 
   def update(conn, _) do
-    text conn, ~s(
+    text(conn, ~s(
       missing secret – proper usage is jdems.us/admin/update?secret=secretfromben
-    )
+    ))
   end
 
-  def default_redirect(path = "/" <> rest) do
+  def default_redirect(_path = "/" <> rest) do
     {int, _} = Integer.parse(rest)
     string_int_string = "#{int}"
 
     if string_int_string == rest do
-      "https://go.justicedemocrats.com/event/event/#{int}"
+      "https://go.justicedemocrats.com/event/event/#{int}?source=shortlink"
     else
       "https://justicedemocrats.com"
     end
